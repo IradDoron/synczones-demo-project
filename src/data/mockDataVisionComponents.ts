4502ee54d8ffcf5bcf9d495738ff412f8de1a75d
Irad Doron <irad.doron@gmail.com>
2023-09-23 12:16:42 +0300

    add vision components

--- src/data/mockDataVisionComponents.ts
+++ src/data/mockDataVisionComponents.ts
@@ -32,6 +32,83 @@ const mockDataVisionComponents: VisionComponent[] = [
 		description: 'Work in a company to gain experience the connections.',
 		relatedVisionsIds: [1],
 	},
+	{
+		id: 6,
+		title: 'Have 10% body fat',
+		description: 'Have 10% body fat to be healthy.',
+		relatedVisionsIds: [2],
+	},
+	{
+		id: 7,
+		title: 'Have big muscles',
+		description: 'Have big muscles in the upper and lower body.',
+		relatedVisionsIds: [2],
+	},
+	{
+		id: 8,
+		title: 'Be strong',
+		description: 'Have a strong body.',
+		relatedVisionsIds: [2],
+	},
+	{
+		id: 9,
+		title: 'Workout regularly',
+		description: 'Workout at least 5 times a week.',
+		relatedVisionsIds: [2],
+	},
+	{
+		id: 10,
+		title: 'Generate a million shekels a year',
+		description:
+			'Generate at least a million shekels a year with combination of passive and active income.',
+		relatedVisionsIds: [3],
+	},
+	{
+		id: 11,
+		title: 'Fill comfortable with myself',
+		description:
+			'Fill comfortable with my pesonality, looks, finances, and health.',
+		relatedVisionsIds: [4],
+	},
+	{
+		id: 12,
+		title: 'Fill comfortable with other people',
+		description: 'Fill comfortable to talk to other people and make friends.',
+		relatedVisionsIds: [4],
+	},
+	{
+		id: 13,
+		title: 'Learn to listen and fill emotions to other people',
+		description:
+			'Learn to listen, empathize, and fill emotions to other people.',
+		relatedVisionsIds: [4],
+	},
+	{
+		id: 14,
+		title: 'Learn to understand and control my emotions',
+		description: 'Manage my emotions in a healthy way.',
+		relatedVisionsIds: [4],
+	},
+	{
+		id: 15,
+		title: 'Master the piano',
+		description:
+			'Master the piano to be able to play any song I want and improvise.',
+		relatedVisionsIds: [5],
+	},
+	{
+		id: 16,
+		title: 'Be a professional at video editing',
+		description: 'Know how to edit videos professionally.',
+		relatedVisionsIds: [5],
+	},
+	{
+		id: 17,
+		title: 'Build and maintain a music learning platform',
+		description:
+			'Build and maintain a music learning platform to help people learn music.',
+		relatedVisionsIds: [5],
+	},
 ];
 
 export default mockDataVisionComponents;
