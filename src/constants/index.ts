428645b9cea19020a60a036824f560396c80915a
Irad Doron <irad.doron@gmail.com>
2023-09-29 12:18:46 +0300

    improve goal display

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,8 @@
 export const STATUS_COLORS = {
 	'Not Started': '#FFFFFF',
+	'Not Started Hover': '#F5F5F5',
 	'In Progress': '#FFC107',
+	'In Progress Hover': '#FFB300',
 	Completed: '#4CAF50',
+	'Completed Hover': '#43A047',
 };
--- src/shared/GoalTasksDisplay/GoalTasksDisplay.tsx
+++ src/shared/GoalTasksDisplay/GoalTasksDisplay.tsx
@@ -7,7 +7,9 @@ import {
 	getCommaSeparatedStringsFromArray,
 	getItemById,
 	getTasksDataFromTasksIds,
+	titleCaseToKebabCase,
 } from '@/utils';
+import Link from 'next/link';
 
 type Props = {
 	goalId: number;
@@ -32,102 +34,120 @@ const GoalTasksDisplay = ({ goalId }: Props) => {
 
 				return (
 					<div key={id}>
-						<table>
-							<caption
-								style={{
-									textAlign: 'left',
-									fontSize: '1.5rem',
-									fontWeight: 'bold',
-									padding: '1rem',
-									backgroundColor: STATUS_COLORS[status],
-								}}
-							>{`${index + 1}. ${title}`}</caption>
-							<thead>
-								<tr>
-									<th>ID</th>
-									<th>Description</th>
-									<th>Related Goals IDs</th>
-									<th>Status</th>
-								</tr>
-							</thead>
-							<tbody>
-								<tr>
-									<td>{id}</td>
-									<td>{description}</td>
-									<td>{getCommaSeparatedStringsFromArray(relatedGoalsIds)}</td>
-									<td
-										style={{
-											backgroundColor: STATUS_COLORS[status],
-										}}
-									>
-										{status}
-									</td>
-								</tr>
-							</tbody>
-						</table>
-						<br />
-						<table>
-							<caption
-								style={{
-									textAlign: 'left',
-									fontSize: '1.5rem',
-									padding: '1rem',
-								}}
-							>
-								Task Steps
-							</caption>
-							<thead>
-								<tr>
-									<th>ID</th>
-									<th>Title</th>
-									<th>Description</th>
-									<th>Status</th>
-								</tr>
-							</thead>
-							<tbody>
-								{taskSteps?.map((taskStep) => {
-									const { id, title, description, status } = taskStep;
-									return (
-										<tr key={id}>
-											<td>{id}</td>
-											<td>{title}</td>
-											<td>{description}</td>
-											<td
-												style={{
-													backgroundColor: STATUS_COLORS[status],
-												}}
-											>
-												{status}
-											</td>
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
-						<br />
-						<br />
-						<br />
+						<details>
+							<summary className={titleCaseToKebabCase(status)}>{`${
+								index + 1
+							}. ${title}`}</summary>
+							<table>
+								<caption className='hidden'>{`${index + 1}. ${title}`}</caption>
+								<thead>
+									<tr>
+										<th>ID</th>
+										<th>Description</th>
+										<th>Related Goals IDs</th>
+										<th>Status</th>
+										<th>Task Link</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>{id}</td>
+										<td>{description}</td>
+										<td>
+											{getCommaSeparatedStringsFromArray(relatedGoalsIds)}
+										</td>
+										<td
+											style={{
+												backgroundColor: STATUS_COLORS[status],
+											}}
+										>
+											{status}
+										</td>
+										<td>
+											<Link href={`/tasks-manager/tasks/${task.url}`}>
+												Link
+											</Link>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+							<br />
+							<table>
+								<caption
+									style={{
+										textAlign: 'left',
+										fontSize: '1.5rem',
+										padding: '1rem',
+									}}
+								>
+									Task Steps
+								</caption>
+								<thead>
+									<tr>
+										<th>ID</th>
+										<th>Title</th>
+										<th>Description</th>
+										<th>Status</th>
+									</tr>
+								</thead>
+								<tbody>
+									{taskSteps?.map((taskStep) => {
+										const { id, title, description, status } = taskStep;
+										return (
+											<tr key={id}>
+												<td>{id}</td>
+												<td>{title}</td>
+												<td>{description}</td>
+												<td
+													style={{
+														backgroundColor: STATUS_COLORS[status],
+													}}
+												>
+													{status}
+												</td>
+											</tr>
+										);
+									})}
+								</tbody>
+							</table>
+						</details>
 					</div>
 				);
 			})}
+			<style jsx>{`
+				summary {
+					font-size: 1.5rem;
+					font-weight: bold;
+					padding: 1rem;
+					cursor: pointer;
+				}
+
+				summary.not-started {
+					background-color: ${STATUS_COLORS['Not Started']};
+				}
+
+				summary.not-started:hover {
+					background-color: ${STATUS_COLORS['Not Started Hover']};
+				}
+
+				summary.in-progress {
+					background-color: ${STATUS_COLORS['In Progress']};
+				}
+
+				summary.in-progress:hover {
+					background-color: ${STATUS_COLORS['In Progress Hover']};
+				}
+
+				summary.completed {
+					background-color: ${STATUS_COLORS['Completed']};
+				}
+
+				summary.completed:hover {
+					background-color: ${STATUS_COLORS['Completed Hover']};
+				}
+			`}</style>
 		</div>
 	);
 };
 
 export default GoalTasksDisplay;
-
-// export type TaskStep = {
-// 	id: number;
-// 	title: string;
-// 	description: string;
-// 	status: 'Completed' | 'In Progress' | 'Not Started';
-// };
-
-// export type Task = {
-// 	id: number;
-// 	title: string;
-// 	description: string;
-// 	relatedGoalsIds: number[];
-// 	taskSteps?: TaskStep[];
-// 	status: 'Completed' | 'In Progress' | 'Not Started';
-// };
