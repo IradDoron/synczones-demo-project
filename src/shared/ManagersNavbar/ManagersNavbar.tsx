86b130ba4771bc2d4caf74e894c7c758780fb46c
Irad Doron <irad.doron@gmail.com>
2023-09-30 10:47:38 +0300

    fix i18n to managers navbar

--- src/shared/ManagersNavbar/ManagersNavbar.tsx
+++ src/shared/ManagersNavbar/ManagersNavbar.tsx
@@ -1,27 +1,52 @@
+import { Locale } from '@/i18n.config';
 import { kebabCaseToTitleCase } from '@/utils';
+import { getDictionary } from '@/utils/dictionary';
 import Link from 'next/link';
 
+type Managers =
+	| 'career-manager'
+	| 'collaboration-manager'
+	| 'finance-manager'
+	| 'goals-manager'
+	| 'health-manager'
+	| 'hobbies-manager'
+	| 'learning-manager'
+	| 'organization-manager'
+	| 'projects-manager'
+	| 'relationships-manager'
+	| 'routines-manager'
+	| 'skills-manager'
+	| 'tasks-manager'
+	| 'time-manager';
+
 type Props = {
-	urls: string[];
-	baseUrl: string;
+	baseUrl: Managers;
+	params: { lang: Locale };
 };
 
-const ManagersNavbar = ({ urls, baseUrl }: Props) => {
+const ManagersNavbar = async ({  baseUrl, params: { lang } }: Props) => {
+	const dictionary = await getDictionary(lang);
+	const manager = baseUrl;
+
+	const urlsEntries = Object.entries(dictionary.app[manager].layout.urls).map(
+		([key, value]) => [key, value]
+	);
+
 	return (
 		<nav>
 			<ul>
-				{urls.map((url) => {
-					const title = kebabCaseToTitleCase(url);
+				{urlsEntries.map((entry) => {
+					const [url, title] = entry;
 					if (url === 'main-page') {
 						return (
 							<li key={url}>
-								<Link href={`${baseUrl}`}>{title}</Link>
+								<Link href={`/${lang}/${baseUrl}`}>{title}</Link>
 							</li>
 						);
 					} else {
 						return (
 							<li key={url}>
-								<Link href={`${baseUrl}/${url}`}>{title}</Link>
+								<Link href={`/${lang}/${baseUrl}/${url}`}>{title}</Link>
 							</li>
 						);
 					}
